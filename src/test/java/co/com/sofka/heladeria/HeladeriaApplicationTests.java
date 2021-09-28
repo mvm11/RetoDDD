2e4f621afd41d51726d5ed4525c24ef6c5895bd9
mvm11 <mvalenciaminota@gmail.com>
2021-09-28 18:45:52 -0500

    The sofka's dependencies have been added

---
+++ src/test/java/co/com/sofka/heladeria/HeladeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.heladeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
