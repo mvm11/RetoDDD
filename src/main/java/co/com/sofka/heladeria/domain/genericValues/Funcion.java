532c524ba7259cb7160efd209c3a8a4ff40eee9f
mvm11 <mvalenciaminota@gmail.com>
2021-09-28 19:08:16 -0500

    The genericValues Funcion, Nombre and Telefono have been added

---
+++ src/main/java/co/com/sofka/heladeria/domain/genericValues/Funcion.java
@@ -0,0 +1,35 @@
+package co.com.sofka.heladeria.domain.genericValues;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Funcion implements ValueObject<String> {
+
+    private final String value;
+
+    public Funcion(String value) {
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("La función no puede estar vacía");
+        }
+    }
+
+    public String value() {
+            return value;
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcion funcion = (Funcion) o;
+        return Objects.equals(value, funcion.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
+
