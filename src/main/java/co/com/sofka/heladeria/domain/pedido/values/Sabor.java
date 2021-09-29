ee42a6ba2e19b04088bb76cecb86f754ae155983
mvm11 <mvalenciaminota@gmail.com>
2021-09-29 17:17:10 -0500

    The object value sabor has been added

--- src/main/java/co/com/sofka/heladeria/domain/pedido/values/Sabor.java
+++ src/main/java/co/com/sofka/heladeria/domain/pedido/values/Sabor.java
@@ -2,8 +2,37 @@ package co.com.sofka.heladeria.domain.pedido.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Sabor implements ValueObject<String> {
     private final String value;
 
+    public Sabor(String value) {
+        this.value = value;
+        if (this.value.isBlank()) {
+            throw new IllegalArgumentException("El sabor no puede estar vacío");
+        }
 
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sabor sabor = (Sabor) o;
+        return Objects.equals(value, sabor.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
