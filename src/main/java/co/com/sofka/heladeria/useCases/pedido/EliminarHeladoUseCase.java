bb426905750184d8a91de5da7657563c60b76a2a
mvm11 <mvalenciaminota@gmail.com>
2021-09-30 02:52:01 -0500

    The useCase EliminiarHeladoUseCase has been added

---
+++ src/main/java/co/com/sofka/heladeria/useCases/pedido/EliminarHeladoUseCase.java
@@ -0,0 +1,18 @@
+package co.com.sofka.heladeria.useCases.pedido;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.heladeria.domain.pedido.Pedido;
+import co.com.sofka.heladeria.domain.pedido.command.EliminarHelado;
+
+public class EliminarHeladoUseCase extends UseCase<RequestCommand<EliminarHelado>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<EliminarHelado> eliminarHeladoRequestCommand) {
+        var command = eliminarHeladoRequestCommand.getCommand();
+        var pedido = Pedido.from(command.getIdPedido(), retrieveEvents(command.getIdHelado().value()));
+        pedido.EliminarHelado(command.getIdPedido(), command.getIdHelado(), command.getSabor());
+        emit().onSuccess(new ResponseEvents(pedido.getUncommittedChanges()));
+    }
+}
