ac25ff59cfaf5b270fb0b12ad395c0b6b3a6abc5
mvm11 <mvalenciaminota@gmail.com>
2021-09-30 02:58:46 -0500

    The UseCase AsignarClienteUseCase has been added

---
+++ src/main/java/co/com/sofka/heladeria/useCases/pedido/AsignarClienteUseCase.java
@@ -0,0 +1,18 @@
+package co.com.sofka.heladeria.useCases.pedido;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.heladeria.domain.pedido.Pedido;
+import co.com.sofka.heladeria.domain.pedido.command.AsignarCliente;
+
+
+public class AsignarClienteUseCase  extends UseCase<RequestCommand<AsignarCliente>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<AsignarCliente> asignarClienteRequestCommand) {
+        var command = asignarClienteRequestCommand.getCommand();
+        var pedido = Pedido.from(command.getIdPedido(), retrieveEvents(command.getIdCliente().value()));
+        pedido.asignarCliente(command.getIdPedido(), command.getIdCliente(), command.getNombre(), command.getTelefono());
+        emit().onSuccess(new ResponseEvents(pedido.getUncommittedChanges()));
+    }
+}
